788304814269f3140ecbbf0ec456fb0e21a3c4c4
nico-sauer <nico.sauer@dci-student.org>
2025-07-24 11:36:01 +0200

    just some small fixes for now

--- config/settings.py
+++ config/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql-psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
--- orders/models.py
+++ orders/models.py
@@ -1,14 +1,21 @@
 from django.db import models
+from users.models import CustomUser
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator
+from shop.models import Product
+
+
+
 
 class Order(models.Model):
-    customer = models.ForeignKey(User, related_name="orders", null=True, on_delete=models.CASCADE)
-    first_name = models.CharField(_('first name'), max_length=50)
-    last_name = models.CharField(_('last name'), max_length=50)
-    email = models.EmailField(_('e-mail'))
-    phone_number = models.CharField(_('phone number'), max_length=20, null=True)
-    address = models.CharField(_('address'), max_length=250)
-    postal_code = models.CharField(_('postal code'), max_length=20)
-    city = models.CharField(_('city'), max_length=100)
+    customer = models.ForeignKey(CustomUser, related_name="orders", null=True, on_delete=models.CASCADE)
+    first_name = models.CharField(('first name'), max_length=50)
+    last_name = models.CharField(('last name'), max_length=50)
+    email = models.EmailField(('e-mail'))
+    phone_number = models.CharField(('phone number'), max_length=20, null=True)
+    address = models.CharField(('address'), max_length=250)
+    postal_code = models.CharField(('postal code'), max_length=20)
+    city = models.CharField(('city'), max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
